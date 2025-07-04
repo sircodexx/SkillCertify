63cb0d709335ab86594f4b0c748fd280e876c590
Miguel <myamunaquevillegas@gmail.com>
2025-07-04 13:57:00 -0500

    Se cambió el nombre de la pagina

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'SkillCertify',
   description: 'Created with v0',
-  generator: 'v0.dev',
+  generator: 'Next.js',
 }
 
 export default function RootLayout({
